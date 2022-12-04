b49860b940c8318b62fd2e5adb508c04dd2bf817
LucasMartinsCeleiro <lucas.martinsc@hes-so.ch>
2022-12-04 13:56:49 +0100

    finish

--- Day3/Backpack.py
+++ Day3/Backpack.py
@@ -2,25 +2,24 @@ backpack = "F:\Python\PROJECT\AdventOfCode2022\Day3\input.txt"
 totalvalue = 0
 
 #cut the string in two egal size
-with open(backpack, mode='r', newline='\n') as file :
+with open(backpack) as file :
     for line in file :
         #Cut the String in two egal part
-        l = len(line)
-        half1 = slice(0,l//2)
-        half2 = slice(l//2,l)
+        lenght = len(line)
+        half1 = line[0:lenght//2]
+        half2 = line[lenght//2:lenght]
 
-        print(line[half1], line[half2])
+        print(half1,half2)
         
         #Find the common char
-        a= ''.join((set(line[half1]).intersection(line[half2])))
-        for i in a:
-            commonChar = i
+        a= ''.join((set(half1).intersection(half2)))
+        for commonChar in a:
         
             #Set the score
-            if commonChar.isupper() == 'False' :
-                totalvalue += ord(commonChar)-96   
+            if commonChar.isupper() :
+                totalvalue += ord(commonChar)-38
             else :
-                totalvalue += ord(commonChar)-64  
+                totalvalue += ord(commonChar)-96
               
     #Print output
     print(totalvalue)    
